32c5a746876845d96bcd25651c5c877cb938d629
Pato <plankenau@gmail.com>
2016-02-17 21:16:36 -0600

    Added method for calculating the lcm

--- euclid.hs
+++ euclid.hs
@@ -3,3 +3,6 @@ euclidGcd a b
     | a > b = euclidGcd b a
     | a == 0 = b
     | otherwise = euclidGcd (b `mod` a) a
+
+euclidLcm :: (Integral a) => a -> a -> a
+euclidLcm a b = (a * b) `div` euclidGcd a b
