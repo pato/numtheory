39f1c448eb809ebb6aae4e51c33dcda77ea46f4c
Pato <plankenau@gmail.com>
2016-02-23 16:03:19 -0600

    Added prime factorization and hw defined algorithm for gcd

--- euclid.hs
+++ euclid.hs
@@ -5,6 +5,14 @@ euclidGcd a b
     | a == 0 = b
     | otherwise = euclidGcd (b `mod` a) a
 
+-- HW defined algorithm for GCD
+hwGCD :: Integral a => a -> a -> a
+hwGCD a b
+    | a == b           = a
+    | even a && even b = 2 * hwGCD (a `div` 2) (b `div` 2)
+    | even a && odd b  = hwGCD (a `div` 2) b
+    | odd a && odd b   = hwGCD ((max a b) - (min a b)) b
+
 -- Get the LCM of two numbers
 euclidLcm :: (Integral a) => a -> a -> a
 euclidLcm a b = (a * b) `div` euclidGcd a b
@@ -26,3 +34,11 @@ multiLcm (x:xs) = euclidLcm x (multiLcm xs)
 -- More succint: solve multiple LCMs
 multiLcm' :: (Integral a) => [a] -> a
 multiLcm' xs = foldl euclidLcm 1 xs
+
+-- Prime factorization of number
+primeFactors :: (Integral a) => a -> [a]
+primeFactors 1 = []
+primeFactors n
+  | factors == []  = [n]
+  | otherwise = factors ++ primeFactors (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
