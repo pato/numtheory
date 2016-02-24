90bccfbabd58e474bb29739063ffd746fce2ee18
Pato <plankenau@gmail.com>
2016-02-24 01:01:22 -0600

    moved primes to its own file

---
+++ primes.hs
@@ -0,0 +1,7 @@
+-- Prime factorization of number
+primeFactors :: (Integral a) => a -> [a]
+primeFactors 1 = []
+primeFactors n
+  | factors == []  = [n]
+  | otherwise = factors ++ primeFactors (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
