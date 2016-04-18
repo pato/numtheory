217a56ce63f78c92ab5542f8d009c91b54d8df4a
Pato <plankenau@gmail.com>
2016-04-18 13:32:43 -0500

    ignoring spaces

--- crypto.hs
+++ crypto.hs
@@ -10,26 +10,36 @@ caesarShiftEnc :: Int -> String -> String
 caesarShiftEnc k plaintext = cyphertext
     where cyphertext = map i2c cypherints
           cypherints = map (\p -> (p + k) `mod` 26) plainints
-          plainints  = map c2i plaintext
+          plainints  = map c2i plaintext'
+          plaintext' = filter (\c -> c /= ' ') plaintext
 
 caesarShiftDec :: Int -> String -> String
 caesarShiftDec k cyphertext = plaintext
-    where plaintext  = map i2c plainints
-          plainints  = map (\p -> (p - k) `mod` 26) cypherints
-          cypherints = map c2i cyphertext
+    where plaintext   = map i2c plainints
+          plainints   = map (\p -> (p - k) `mod` 26) cypherints
+          cypherints  = map c2i cyphertext'
+          cyphertext' = filter (\c -> c /= ' ') cyphertext
 
 affineShiftEnc :: Int -> Int -> String -> String
 affineShiftEnc a k plaintext = cyphertext
     where cyphertext = map i2c cypherints
           cypherints = map (\p -> (a * p + k) `mod` 26) plainints
-          plainints  = map c2i plaintext
+          plainints  = map c2i plaintext'
+          plaintext' = filter (\c -> c /= ' ') plaintext
 
 affineShiftDec :: Int -> Int -> String -> String
 affineShiftDec a k cyphertext = plaintext
-    where plaintext  = map i2c plainints
-          plainints  = map (\p -> a' * (p - k) `mod` 26) cypherints
-          cypherints = map c2i cyphertext
+    where plaintext   = map i2c plainints
+          plainints   = map (\p -> a' * (p - k) `mod` 26) cypherints
+          cypherints  = map c2i cyphertext'
+          cyphertext' = filter (\c -> c /= ' ') cyphertext
           a' = congInv a 26
+
+-- guess what the letters that correspond to e and t are and try to decode
+-- affineGuess :: Char -> Char -> String -> String
+
+bruteForce :: String -> [(Int, Int, String)]
+bruteForce cyphertext = map (\(a,k) -> (a,k,affineShiftDec a k cyphertext)) [(x,y) | x <- [1..26], y <- [1..26]]
 
 freqDist :: String -> [(Char, Int)]
 freqDist text = sorted
