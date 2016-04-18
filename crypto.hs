3aa1a88f838e39ac9cf1138bb01fdeef36ae0cb1
Pato <plankenau@gmail.com>
2016-04-18 12:16:28 -0500

    Added decoding functions

--- crypto.hs
+++ crypto.hs
@@ -1,5 +1,6 @@
 module Crypto where
 
+import Congruences
 import Data.Char
 import Data.String
 
@@ -9,12 +10,27 @@ caesarShiftEnc k plaintext = cyphertext
           cypherints = map (\p -> (p + k) `mod` 26) plainints
           plainints  = map c2i plaintext
 
+caesarShiftDec :: Int -> String -> String
+caesarShiftDec k cyphertext = plaintext
+    where plaintext  = map i2c plainints
+          plainints  = map (\p -> (p - k) `mod` 26) cypherints
+          cypherints = map c2i cyphertext
+
 affineShiftEnc :: Int -> Int -> String -> String
 affineShiftEnc a k plaintext = cyphertext
     where cyphertext = map i2c cypherints
           cypherints = map (\p -> (a * p + k) `mod` 26) plainints
           plainints  = map c2i plaintext
 
+affineShiftDec :: Int -> Int -> String -> String
+affineShiftDec a k cyphertext = plaintext
+    where plaintext  = map i2c plainints
+          plainints  = map (\p -> a' * (p - k) `mod` 26) cypherints
+          cypherints = map c2i cyphertext
+          a' = congInv a 26
+
+
+-- encodeByChar (Char -> Char) -> String -> String
 
 c2i :: Char -> Int
 c2i c = ord c - ord 'a'
